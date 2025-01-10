0f9f8c19deb005449e8e2bc440e67be5297cf80d
yeison <yeisonchuleta@gmail.com>
2025-01-10 16:01:45 -0400

    mejora de la funcionalidad y estilos

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const btn_menu_responsive = document.querySelector('.btn_menu_responsive');
 const btn_close = document.querySelector('.btn_close');
 const menu_mobile = document.querySelector('.menu_mobile');
 const corazon = document.getElementById('corazon');
+const corazonn = document.querySelectorAll(".corazonn")
+const like = document.getElementById('like')
+const heart = document.querySelectorAll('fa-heart')
 
 
 btn_menu_responsive.addEventListener('click', () => {
@@ -14,4 +17,24 @@ btn_close.addEventListener('click', () => {
 
 corazon.addEventListener('click', () => {
     corazon.classList.toggle('active');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    
+    Toast.fire({
+        icon: "success",
+        title: "Acción realizada con éxito"
+    });
 });
+
+heart.addEventListener('click',() =>{
+    fa-heart.classList.toggle('active')
+})
